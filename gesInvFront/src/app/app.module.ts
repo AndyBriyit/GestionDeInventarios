8642a9d49a0d244f2e482474932549c1fa19acd3
AndyBriyit12 <briyit@clandigital.com>
2024-11-24 11:14:52 -0500

    agregando modulos

--- gesInvFront/src/app/app.module.ts
+++ gesInvFront/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductFormComponent } from './pages/product-form/product-form.component';
+import { InventoryTableComponent } from './pages/inventory-table/inventory-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProductFormComponent,
+    InventoryTableComponent
   ],
   imports: [
     BrowserModule,
