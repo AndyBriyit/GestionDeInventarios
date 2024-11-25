49b81753df8f8d08835e34aeb52c485be37e7f5f
AndyBriyit12 <briyit@clandigital.com>
2024-11-25 13:16:42 -0500

    agregando rutas para los compoenentes

--- gesInvFront/src/app/app.module.ts
+++ gesInvFront/src/app/app.module.ts
@@ -1,22 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProductFormComponent } from './pages/product-form/product-form.component';
 import { InventoryTableComponent } from './pages/inventory-table/inventory-table.component';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ProductViewComponent } from './pages/product-view/product-view.component';
+import { ConfirmDialogComponent } from './pages/confirm-dialog/confirm-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ProductFormComponent,
-    InventoryTableComponent
+    InventoryTableComponent,
+    ProductViewComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatInputModule,
+    MatSortModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatDialogModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
