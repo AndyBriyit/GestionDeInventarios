75fb7d7fcf171c8bfad6d666f4f2d0aae38e1e53
AndyBriyit12 <briyit@clandigital.com>
2024-11-25 13:15:29 -0500

    agregando rutas de los componentes

--- gesInvFront/src/app/app-routing.module.ts
+++ gesInvFront/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component'; // Importar el componente de login
 import { InventoryTableComponent } from './pages/inventory-table/inventory-table.component'; // Importar componente de consulta
-
 import { ProductFormComponent } from './pages/product-form/product-form.component'; // Importar componente de creación de productos
+import { ProductViewComponent } from './pages/product-view/product-view.component';
+
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' }, // Ruta por defecto redirige al login
   { path: 'login', component: LoginComponent }, // Ruta para el login
   { path: 'inventory', component: InventoryTableComponent }, // Ruta para consulta del inventario
-  { path: 'create-product', component: ProductFormComponent }, // Ruta para registrar productos
+  { path: 'product-form', component: ProductFormComponent }, // Ruta para registrar productos
+  { path: 'product-view/:id', component: ProductViewComponent },
 ];
 
 @NgModule({
